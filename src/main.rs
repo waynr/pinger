2fd7b8568f84cde25c85aa3e0c2d729d3a93c3e9
wayne warren <wayne.warren.s@gmail.com>
2023-09-09 10:53:28 -0600

    factor socket creation out of IcmpSocket::new

--- src/main.rs
+++ src/main.rs
@@ -70,13 +70,68 @@ impl Pinger {
     fn new(ethernet_conf: EthernetConf, rb_size: usize, icmp_timeout: Duration) -> Result<Self> {
         let socket_rb: ArrayQueue<Box<IcmpSocket>> = ArrayQueue::new(rb_size);
         for _ in 0..rb_size {
-            let icmp_socket = Box::new(IcmpSocket::new(&ethernet_conf, icmp_timeout.clone())?);
+            let inner = Self::create_socket(&ethernet_conf)?;
+            let icmp_socket = Box::new(IcmpSocket::new(inner, &ethernet_conf, icmp_timeout.clone())?);
             socket_rb
                 .push(icmp_socket)
                 .expect("don't push more than the queues capacity");
         }
 
         Ok(Self { socket_rb })
+    }
+
+    fn create_socket(ethernet_conf: &EthernetConf) -> Result<Socket> {
+        // choose Domain::PACKET here so that we can cache ICMP reply packets and circumvent
+        // network-layer handling of packets in the kernel
+        let socket = Socket::new(
+            Domain::PACKET,
+            Type::RAW,
+            Some(Protocol::from(libc::ETH_P_ALL.to_be())),
+        )?;
+
+        // initialize sockaddr_storage then reference as raw pointer to a sockaddr_ll in order to
+        // set link-layer options on the addr before binding the socket. the intent here is to bind
+        // the AF_PACKET socket by index to the interface in the given EthernetConf.
+        //
+        // it's safe to initialize a sockaddr_storage to all zeroes because zeroes are valid values
+        // for its fields.
+        let mut addr_storage: libc::sockaddr_storage = unsafe { std::mem::zeroed() };
+        {
+            // I don't really understand why this line isn't considered unsafe by the compiler
+            let mut addr_ll_ref: *mut libc::sockaddr_ll =
+                (&mut addr_storage as *mut libc::sockaddr_storage).cast();
+
+            // these operations are safe because according to 'man sockaddr_storage' it is designed
+            // to be at least as large as any other sockaddr_* libc type and cast to any of those
+            // types (such as sockaddr_ll here) so that the needed fields for the sockaddr_* type
+            // can be set
+            //let hw = ethernet_conf.ethernet_info.source.clone();
+            //log::debug!("hw addr: {:?}", hw);
+            unsafe {
+                (*addr_ll_ref).sll_family = libc::AF_PACKET as u16;
+                (*addr_ll_ref).sll_ifindex = ethernet_conf.interface.index as i32;
+                (*addr_ll_ref).sll_protocol = libc::ETH_P_ALL as u16;
+                //(*addr_ll_ref).sll_addr = [hw.0, hw.1, hw.2, hw.3, hw.4, hw.5, 0, 0];
+                log::debug!("sockaddr_ll for bind set to: {:?}", *addr_ll_ref);
+            }
+        }
+        let len = std::mem::size_of_val(&addr_storage) as libc::socklen_t;
+
+        // the following is safe because of the abovementioned explanations regarding initializing
+        // the sockaddr_storage bits to 0 and casting to a sockaddr_ll to set link-layer fields for
+        // the sockaddr; so we have correctly constructed our sockaddr_storage.
+        let addr = unsafe { SockAddr::new(addr_storage, len) };
+
+        // trying to bind any other type of SockAddr (eg Ipv4Addr) than what we have initialized
+        // above would fail with an EINVAL error for an AF_PACKET
+        socket.bind(&addr)?;
+
+        socket.set_nonblocking(true)?;
+        let rw_timeout = Some(Duration::from_millis(1));
+        socket.set_write_timeout(rw_timeout)?;
+        socket.set_read_timeout(rw_timeout)?;
+
+        Ok(socket)
     }
 
     async fn ping(&self, addr: &SockAddr, seq: u16) {
@@ -136,56 +191,7 @@ struct IcmpSocket {
 }
 
 impl IcmpSocket {
-    fn new(ethernet_conf: &EthernetConf, icmp_timeout: Duration) -> Result<Self> {
-        // choose Domain::PACKET here so that we can cache ICMP reply packets and circumvent
-        // network-layer handling of packets in the kernel
-        let inner = Socket::new(
-            Domain::PACKET,
-            Type::RAW,
-            Some(Protocol::from(libc::ETH_P_ALL.to_be())),
-        )?;
-
-        // initialize sockaddr_storage then reference as raw pointer to a sockaddr_ll in order to
-        // set link-layer options on the addr before binding the socket. the intent here is to bind
-        // the AF_PACKET socket by index to the interface in the given EthernetConf.
-        //
-        // it's safe to initialize a sockaddr_storage to all zeroes because zeroes are valid values
-        // for its fields.
-        let mut addr_storage: libc::sockaddr_storage = unsafe { std::mem::zeroed() };
-        {
-            // I don't really understand why this line isn't considered unsafe by the compiler
-            let mut addr_ll_ref: *mut libc::sockaddr_ll =
-                (&mut addr_storage as *mut libc::sockaddr_storage).cast();
-
-            // these operations are safe because according to 'man sockaddr_storage' it is designed
-            // to be at least as large as any other sockaddr_* libc type and cast to any of those
-            // types (such as sockaddr_ll here) so that the needed fields for the sockaddr_* type
-            // can be set
-            //let hw = ethernet_conf.ethernet_info.source.clone();
-            //log::debug!("hw addr: {:?}", hw);
-            unsafe {
-                (*addr_ll_ref).sll_family = libc::AF_PACKET as u16;
-                (*addr_ll_ref).sll_ifindex = ethernet_conf.interface.index as i32;
-                (*addr_ll_ref).sll_protocol = libc::ETH_P_ALL as u16;
-                //(*addr_ll_ref).sll_addr = [hw.0, hw.1, hw.2, hw.3, hw.4, hw.5, 0, 0];
-                log::debug!("sockaddr_ll for bind set to: {:?}", *addr_ll_ref);
-            }
-        }
-        let len = std::mem::size_of_val(&addr_storage) as libc::socklen_t;
-
-        // the following is safe because of the abovementioned explanations regarding initializing
-        // the sockaddr_storage bits to 0 and casting to a sockaddr_ll to set link-layer fields for
-        // the sockaddr; so we have correctly constructed our sockaddr_storage.
-        let addr = unsafe { SockAddr::new(addr_storage, len) };
-
-        // trying to bind any other type of SockAddr (eg Ipv4Addr) than what we have initialized
-        // above would fail with an EINVAL error for an AF_PACKET
-        inner.bind(&addr)?;
-
-        inner.set_nonblocking(true)?;
-        let rw_timeout = Some(Duration::from_millis(1));
-        inner.set_write_timeout(rw_timeout)?;
-        inner.set_read_timeout(rw_timeout)?;
+    fn new(socket: Socket, ethernet_conf: &EthernetConf, icmp_timeout: Duration) -> Result<Self> {
 
         let mut buf = [0u8; ICMP_REQUEST_PACKET_SIZE];
         {
@@ -233,7 +239,7 @@ impl IcmpSocket {
         }
 
         Ok(Self {
-            inner: AsyncFd::new(inner)?,
+            inner: AsyncFd::new(socket)?,
             buf,
             icmp_timeout,
         })
