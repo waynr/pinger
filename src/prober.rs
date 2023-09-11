2978c000d64bafa9e992fcae3097e8d4edd9ac6c
wayne warren <wayne.warren.s@gmail.com>
2023-09-11 00:30:41 -0600

    eliminate unused IcmpProber struct

--- src/prober.rs
+++ src/prober.rs
@@ -147,11 +147,11 @@ pub struct Prober<P: Probe + Send + Sync + 'static + std::fmt::Debug> {
 
 impl<P: Probe + Send + Sync + 'static + std::fmt::Debug> Prober<P> {
     pub fn new(probes: Vec<P>, ethernet_conf: EthernetConf, timeout: Duration) -> Result<Self> {
-        let sender = IcmpProber::create_sender(&ethernet_conf)?;
+        let sender = create_sender(&ethernet_conf)?;
 
         let queue = ArrayQueue::new(probes.len());
         for probe in probes {
-            let receiver = IcmpProber::create_receiver()?;
+            let receiver = create_receiver()?;
             let probe_task = ProbeTask {
                 probe: Arc::new(Mutex::new(probe)),
                 sender: sender.clone(),
@@ -182,7 +182,7 @@ impl<P: Probe + Send + Sync + 'static + std::fmt::Debug> Prober<P> {
             Err(e) => {
                 eprintln!("{} failed: {:?}", tparams, e);
                 None
-            },
+            }
         };
 
         loop {
@@ -224,70 +224,66 @@ impl<P: Probe + Send + Sync + 'static + std::fmt::Debug> Prober<P> {
 //
 // [1] https://docs.rs/crossbeam/latest/crossbeam/queue/struct.SegQueue.html
 //
-struct IcmpProber { }
+fn create_receiver() -> Result<AsyncSocket> {
+    // note: for some reason using Domain::PACKET as is done in zmap (libpcap, really) doesn't
+    // work here -- the socket never becomes ready for reading. for now I'm setting it back to
+    // Domain::IPV4 but will continue trying to figure out how to get Domain::PACKET working
+    let socket = Socket::new(Domain::IPV4, Type::RAW, Some(Protocol::ICMPV4))?;
 
-impl IcmpProber {
-    fn create_receiver() -> Result<AsyncSocket> {
-        // note: for some reason using Domain::PACKET as is done in zmap (libpcap, really) doesn't
-        // work here -- the socket never becomes ready for reading. for now I'm setting it back to
-        // Domain::IPV4 but will continue trying to figure out how to get Domain::PACKET working
-        let socket = Socket::new(Domain::IPV4, Type::RAW, Some(Protocol::ICMPV4))?;
+    socket.set_nonblocking(true)?;
+    let rw_timeout = Some(Duration::from_millis(1));
+    socket.set_write_timeout(rw_timeout)?;
+    socket.set_read_timeout(rw_timeout)?;
 
-        socket.set_nonblocking(true)?;
-        let rw_timeout = Some(Duration::from_millis(1));
-        socket.set_write_timeout(rw_timeout)?;
-        socket.set_read_timeout(rw_timeout)?;
+    Ok(AsyncSocket::new(socket)?)
+}
 
-        Ok(AsyncSocket::new(socket)?)
-    }
+fn create_sender(ethernet_conf: &EthernetConf) -> Result<AsyncSocket> {
+    // choose Domain::PACKET here so that we can cache ICMP reply packets and circumvent
+    // network-layer handling of packets in the kernel
+    let socket = Socket::new(Domain::PACKET, Type::RAW, None)?;
 
-    fn create_sender(ethernet_conf: &EthernetConf) -> Result<AsyncSocket> {
-        // choose Domain::PACKET here so that we can cache ICMP reply packets and circumvent
-        // network-layer handling of packets in the kernel
-        let socket = Socket::new(Domain::PACKET, Type::RAW, None)?;
+    socket.set_nonblocking(true)?;
+    let rw_timeout = Some(Duration::from_millis(1));
+    socket.set_write_timeout(rw_timeout)?;
+    socket.set_read_timeout(rw_timeout)?;
 
-        socket.set_nonblocking(true)?;
-        let rw_timeout = Some(Duration::from_millis(1));
-        socket.set_write_timeout(rw_timeout)?;
-        socket.set_read_timeout(rw_timeout)?;
+    // initialize sockaddr_storage then reference as raw pointer to a sockaddr_ll in order to
+    // set link-layer options on the addr before binding the socket. the intent here is to bind
+    // the AF_PACKET socket by index to the interface in the given EthernetConf.
+    //
+    // it's safe to initialize a sockaddr_storage to all zeroes because zeroes are valid values
+    // for its fields.
+    let mut addr_storage: libc::sockaddr_storage = unsafe { std::mem::zeroed() };
+    {
+        // I don't really understand why this line isn't considered unsafe by the compiler
+        let mut addr_ll_ref: *mut libc::sockaddr_ll =
+            (&mut addr_storage as *mut libc::sockaddr_storage).cast();
 
-        // initialize sockaddr_storage then reference as raw pointer to a sockaddr_ll in order to
-        // set link-layer options on the addr before binding the socket. the intent here is to bind
-        // the AF_PACKET socket by index to the interface in the given EthernetConf.
-        //
-        // it's safe to initialize a sockaddr_storage to all zeroes because zeroes are valid values
-        // for its fields.
-        let mut addr_storage: libc::sockaddr_storage = unsafe { std::mem::zeroed() };
-        {
-            // I don't really understand why this line isn't considered unsafe by the compiler
-            let mut addr_ll_ref: *mut libc::sockaddr_ll =
-                (&mut addr_storage as *mut libc::sockaddr_storage).cast();
-
-            // these operations are safe because according to 'man sockaddr_storage' it is designed
-            // to be at least as large as any other sockaddr_* libc type and cast to any of those
-            // types (such as sockaddr_ll here) so that the needed fields for the sockaddr_* type
-            // can be set
-            //let hw = ethernet_conf.ethernet_info.source.clone();
-            //log::debug!("hw addr: {:?}", hw);
-            unsafe {
-                (*addr_ll_ref).sll_family = libc::AF_PACKET as u16;
-                (*addr_ll_ref).sll_ifindex = ethernet_conf.interface.index as i32;
-                (*addr_ll_ref).sll_protocol = libc::ETH_P_ALL as u16;
-                //(*addr_ll_ref).sll_addr = [hw.0, hw.1, hw.2, hw.3, hw.4, hw.5, 0, 0];
-                log::debug!("sockaddr_ll for bind set to: {:?}", *addr_ll_ref);
-            }
+        // these operations are safe because according to 'man sockaddr_storage' it is designed
+        // to be at least as large as any other sockaddr_* libc type and cast to any of those
+        // types (such as sockaddr_ll here) so that the needed fields for the sockaddr_* type
+        // can be set
+        //let hw = ethernet_conf.ethernet_info.source.clone();
+        //log::debug!("hw addr: {:?}", hw);
+        unsafe {
+            (*addr_ll_ref).sll_family = libc::AF_PACKET as u16;
+            (*addr_ll_ref).sll_ifindex = ethernet_conf.interface.index as i32;
+            (*addr_ll_ref).sll_protocol = libc::ETH_P_ALL as u16;
+            //(*addr_ll_ref).sll_addr = [hw.0, hw.1, hw.2, hw.3, hw.4, hw.5, 0, 0];
+            log::debug!("sockaddr_ll for bind set to: {:?}", *addr_ll_ref);
         }
-        let len = std::mem::size_of_val(&addr_storage) as libc::socklen_t;
-
-        // the following is safe because of the abovementioned explanations regarding initializing
-        // the sockaddr_storage bits to 0 and casting to a sockaddr_ll to set link-layer fields for
-        // the sockaddr; so we have correctly constructed our sockaddr_storage.
-        let addr = unsafe { SockAddr::new(addr_storage, len) };
-
-        // trying to bind any other type of SockAddr (eg Ipv4Addr) than what we have initialized
-        // above would fail with an EINVAL error for an AF_PACKET
-        socket.bind(&addr)?;
-
-        Ok(AsyncSocket::new(socket)?)
     }
+    let len = std::mem::size_of_val(&addr_storage) as libc::socklen_t;
+
+    // the following is safe because of the abovementioned explanations regarding initializing
+    // the sockaddr_storage bits to 0 and casting to a sockaddr_ll to set link-layer fields for
+    // the sockaddr; so we have correctly constructed our sockaddr_storage.
+    let addr = unsafe { SockAddr::new(addr_storage, len) };
+
+    // trying to bind any other type of SockAddr (eg Ipv4Addr) than what we have initialized
+    // above would fail with an EINVAL error for an AF_PACKET
+    socket.bind(&addr)?;
+
+    Ok(AsyncSocket::new(socket)?)
 }
