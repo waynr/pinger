e9a0aeee8f0bb6840867dc046f30f040b2a7e173
wayne warren <wayne.warren.s@gmail.com>
2023-09-11 10:20:56 -0600

    probes/icmp: remove old docstring

--- src/probes/icmp.rs
+++ src/probes/icmp.rs
@@ -22,29 +22,7 @@ const ICMP_REQUEST_PACKET_SIZE: usize = ETHERNET_PACKET_MIN_SIZE
 //TODO: check reply packet size in validation method
 const ICMP_REPLY_PACKET_SIZE: usize = EchoReplyPacket::minimum_packet_size();
 
-/// Abstraction for containing individual socket instances and pre-allocated Ethernet packet
-/// buffer.
-///
-/// # Notes on Socket choice:
-///
-/// The reason I choose Domain::PACKET is that I have been reading the `zmap` paper recently[1] and
-/// learned one of the tricks they use to achieve such high packet throughput is to use AF_PACKET
-/// and manually construct Ethernet packets. This has two primary benefits:
-///
-/// First, it bypasses TCP/IP/whatever handling at the kernel level. This reduces kernel-specific
-/// cpu and memory overhead in high throughput applications.
-///
-/// Second, in network mapping (or ICMP echo as is the case here) there are often very few
-/// differences between the different request packets, which means one can optimize the
-/// program to reduce memory allocations by pre-allocating request packet buffers rather
-/// constructing them for every request. Request packet buffers are only needed for the duration of
-/// each send call, after which they can re-used for subsequent requests.
-///
-/// To be honest, for a simple exercise like this the memory allocation optimization probably isn't
-/// necessary, but I've been wondering how I would implement something zmap-like in Rust while
-/// reading the original zmap paper and this interview is a good chance to do that.
-///
-/// [1] https://zmap.io/paper.pdf
+/// Implementation of `Probe` trait to enable a `Prober` to conduct ICMP echo probes.
 #[derive(Debug)]
 pub struct IcmpProbe {
     buf: [u8; ICMP_REQUEST_PACKET_SIZE],
