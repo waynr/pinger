1323db5fcaf018f789756b602038febd18131eb3
wayne warren <wayne.warren.s@gmail.com>
2023-09-19 17:04:47 -0600

    error: remove unnecessary Error variants

--- src/error.rs
+++ src/error.rs
@@ -16,19 +16,7 @@ pub enum Error {
     RtnetlinkError(#[from] rtnetlink::Error),
 
     #[error("{0:?}")]
-    AsyncChannelRecvError(#[from] async_channel::RecvError),
-
-    #[error("{0:?}")]
-    AsyncChannelTryRecvError(#[from] async_channel::TryRecvError),
-
-    #[error("{0:?}")]
     TokioJoinError(#[from] tokio::task::JoinError),
-
-    #[error("{0:?}")]
-    AsyncChannelTargetParamsSendError(#[from] async_channel::SendError<super::prober::TargetParams>),
-
-    #[error("{0:?}")]
-    StdMpscTryRecvError(#[from] std::sync::mpsc::TryRecvError),
 
     #[error("failed to send output on output handler channel")]
     OutputHandlerChannelClosed,
